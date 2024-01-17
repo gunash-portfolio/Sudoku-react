27cee7e08fb2cac6a56f0d75dd34a0259039ae15
Gunesh Farzaliyev <guneshfarzaliyev@Guneshs-MacBook-Pro.local>
2024-01-17 14:00:22 +0400

    SudokuCellProps is created

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+interface SudokuCellProps {
+  index:number;
+  value:number;
+  onChange:(index:number,value:string)=>void;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <h1>New project</h1>
   );
 }
 
